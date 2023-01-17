793c67d3d07a5e37c25e7705da85662e0d539c42
Astha Varshney <astha119btcse21@igdtuw.ac.in>
2023-01-17 19:14:29 +0530

    user instance

--- blog/serializer.py
+++ blog/serializer.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model=User
     fields=['id', 'username']
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username']
